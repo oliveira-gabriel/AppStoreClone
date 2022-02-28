d8e8dfcbca4f9b7822de7fb3f7c97521112116fe
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-02-28 15:18:23 -0300

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Gabriel Oliveira on 28/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
