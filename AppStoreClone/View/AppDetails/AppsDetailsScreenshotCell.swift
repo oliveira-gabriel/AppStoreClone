5297494dbc5147463a1f4c749db6302f85c88ae0
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-01 11:25:28 -0300

    feat: add AppDetailsDescriptionCell

---
+++ AppStoreClone/View/AppDetails/AppsDetailsScreenshotCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+class AppDetailsScreenshotCell: UICollectionViewCell {
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
