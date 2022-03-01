8474c81973353fce842b22bca837cb31c28fa4d3
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-01 10:35:01 -0300

    feat: add view AppDetailsHeaderCell

---
+++ AppStoreClone/View/AppDetails/AppDetailsHearderCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+class AppDetailsHeaderCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
