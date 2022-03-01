cb95e7d339af1c09c5dd8a643d2538343b3ae53b
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-01 11:10:39 -0300

    feat: add DescriptionStackView

--- AppStoreClone/View/AppDetails/AppDetailsDescriptionCell.swift
+++ AppStoreClone/View/AppDetails/AppDetailsDescriptionCell.swift
@@ -3,10 +3,20 @@ import UIKit
 
 class AppDetailsDescriptionCell: UICollectionViewCell {
 
-
-
+    let titleLabel: UILabel = .textBoldLabel(text: "Novidades", fontSize: 24)
+    let descriptionLabel: UILabel = .textLabel(text: "Descrição", fontSize: 12, numberOfLine: 0)
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        stackView.spacing = 12
+        stackView.axis = .vertical
+
+
+
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 20, bottom: 0, right: 20))
     }
 
     required init?(coder: NSCoder) {
